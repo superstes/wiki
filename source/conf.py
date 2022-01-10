e495d6c5ce050290b0beb5453c61dbcba457a966
René Rath <rene.rath@superstes.eu>
2022-01-10 20:15:50 +0100

    init

---
+++ source/conf.py
@@ -0,0 +1,16 @@
+project = 'Superstes WIKI'
+copyright = '2022, Superstes'
+author = 'Superstes'
+extensions = ['sphinx_rtd_theme']
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {}
+html_static_path = ['_static']
+#html_logo = '_static/img/logo.svg'
+#html_favicon = 'img/favicon.ico'
+html_css_files = ['css/wiki.css']
+master_doc = 'index'
+display_version = True
+sticky_navigation = True
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,8 @@
+Welcome to Superstes WIKI
+=========================
+
+Open Source knowledge for everyone.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
